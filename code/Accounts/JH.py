4bbfeef9bf60c9fda4c9b057a6e7f2c9243271a1
Johyun <porory415@gmail.com>
2021-08-02 14:22:27 +0900

    [21.08.02] fill Sales Person_JH

--- code/Accounts/JH.py
+++ code/Accounts/JH.py
@@ -5,6 +5,9 @@ from openpyxl import Workbook
 # zcrm_1920545000000117001 == Arun Sharma
 # zcrm_1920545000001530001 == Mahesh Gulwani
 # zcrm_1920545000011182001 == Taukheer Ahmed
+# zcrm_1920545000000243011 ==Karn Sharma
+# zcrm_1920545000000097003 == Karn Sharma
+# zcrm_1920545000000097003 == Anees Mukhtar ==zcrm_1920545000001670001
 
 # wb = openpyxl.Workbook("../code/Accounts/Accounts_001_droppedCol2.csv")
 # ws = wb.active
@@ -21,15 +24,14 @@ dfs = pd.read_csv("../../resource/CleansedData/ParsedData/Accounts_001_droppedCo
 for i in dfs.index :
     if dfs['Company Owner ID'][i] == 'zcrm_1920545000000117001':
         dfs['Sales Person'][i] = 'Arun Sharma'
+    if dfs['Company Owner ID'][i] == 'zcrm_1920545000001530001':
+        dfs['Sales Person'][i] = 'Mahesh Gulwani'
+    if dfs['Company Owner ID'][i] == 'zcrm_1920545000011182001':
+        dfs['Sales Person'][i] = 'Taukheer Ahmed'
+    if dfs['Company Owner ID'][i] == 'zcrm_1920545000000243011':
+        dfs['Sales Person'][i] = 'Karn Sharma'
+    if dfs['Company Owner ID'][i] == 'zcrm_1920545000001670001':
+        dfs['Sales Person'][i] = 'Anees Mukhtar'
 
-    # if df['Company Owner ID'] == 'zcrm_1920545000000117001' :
-    #     df['Sales Person'] = 'Arun Sharma'
-    # if df == 'zcrm_1920545000001530001' :
-    #     dfs['Sales Person'][num] = 'Mahesh Gulwani'
-    # if df == 'zcrm_1920545000011182001' :
-    #     dfs['Sales Person'][num] = 'Taukheer Ahmed'
+dfs = dfs.to_csv("Accounts.csv")
 
-# df[(df['Company Owner ID']=='zcrm_1920545000000117001')]['Sales person'] ='Arun Sharma'
-
-
-# dfs.to_csv('Accounts_001_droppedCol2.csv',index =False)
