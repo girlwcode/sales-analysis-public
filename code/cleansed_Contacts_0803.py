94e7b795d461b66cf85a3ca481caaf8fbc240141
MinYeong-37 <min__37@naver.com>
2021-08-03 10:59:57 +0900

    [21.08.03] Accounts_concat_state_MY

--- code/cleansed_Contacts_0803.py
+++ code/cleansed_Contacts_0803.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-contacts = pd.read_csv('../resource/CleansedData/ModifiedData/Contacts_001_droppedCol_deleted_korean.csv')
+contacts = pd.read_csv('../resource/CleansedData/ModifiedData/Contacts_001_deleted_korean.csv')
 accounts = pd.read_csv('../resource/CleansedData/ModifiedData/Accounts_001_concat_state.csv')
 deal = pd.read_csv('../resource/CleansedData/ModifiedData/Potentials_001_USDtoINR.csv')
 
-print(accounts['Territories'].unique())
+# print(accounts['Territories'].unique())
 
 # 3134
-print(accounts['Territories'].isna().sum())
+# print(accounts['Territories'].isna().sum())
 # 1408
-print(contacts['Territories'].isna().sum())
+# print(contacts['Territories'].isna().sum())
 
 # 23 'Arun Sharma' 'Mahesh Gulwani' 'Taukheer Ahmed' 'Karn Sharma'
 #  'Ashwini Dixit' 'Anees Mukhtar' 'Shubham Tonk' 'Ayan' 'CS/Amazon/Other'
@@ -17,14 +17,14 @@ print(contacts['Territories'].isna().sum())
 #  'Mrunal Sardar' 'Mohammad Afnan' 'Gurraj Singh' 'Kenneth Cha'
 #  'Prasad Gosavi' 'Ankita' 'Abhinanda Ghosh' 'Puneet Shukla'
 #  'Neelabh Kashyap'
-print(accounts['Sales Person'].nunique(),accounts['Sales Person'].unique())
+# print(accounts['Sales Person'].nunique(),accounts['Sales Person'].unique())
 
 # 22 'Karn Sharma' 'Arun Sharma' 'Mahesh Gulwani' 'Taukheer Ahmed'
 #  'Kenneth Cha' 'Anees Mukhtar' 'Shubham Tonk' 'Ashwini Dixit'
 #  'Nihal Pawar' 'Nasir' 'Dixita Sharma' 'Ayan' 'Chris Shin' 'Mrunal Sardar'
 #  'Afnan Mohammed' 'Gurraj Singh' 'Aaruni Sinha' 'Prasad Gosavi'
 #  'Puneet Shukla' 'Ankita' 'Neelabh Kashyap' 'Abhinanda Ghosh'
-print(contacts['Sales Person'].nunique(), contacts['Sales Person'].unique())
+# print(contacts['Sales Person'].nunique(), contacts['Sales Person'].unique())
 
 territories = {}
 owner_id = {}
@@ -48,7 +48,7 @@ for name in territories:
 
 # 2. id 별 territory
 for i in contacts.index:
-    if pd.notnull(contacts.loc[i, 'Territories']) :
+    if pd.notnull(contacts.loc[i, 'Territories']):
         id = contacts.loc[i, 'Customer Owner ID']
         territory = contacts.loc[i, 'Territories']
         if id not in owner_id:
@@ -59,7 +59,7 @@ for i in contacts.index:
 id_dict = {}
 for id in owner_id:
     if len(set(owner_id[id])) == 1:
-        print(id, ':', set(owner_id[id]))
+        # print(id, ':', set(owner_id[id]))
         id_dict[id] = owner_id[id][0]
 
 # 1. 직원별 territory dict 고유값 채워주기 - 3개
@@ -83,9 +83,12 @@ for i in contacts.index:
 # 3. deal의 CompanyID와 연동하여 territory 찾기
 company_id = {}
 for i in contacts.index:
-    if contacts['Territories'][i].isna():
-        break
+    c_id = contacts.loc[i, 'Company ID']
+    territory = contacts.loc[i, 'Territories']
+    if c_id not in territories:
+        company_id[c_id] = []
+    company_id[c_id].append(territory)
 
 
 # Parsing the dataset
-contacts.to_csv('../resource/CleansedData/Contacts_001_fillTerritory_ver1.csv', index=False)
+# contacts.to_csv('../resource/CleansedData/Contacts_001_fillTerritory_ver1.csv', index=False)
