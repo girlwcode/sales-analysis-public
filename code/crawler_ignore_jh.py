ff104a8d155c8b6ad398d85a26f38891d8f9a3be
Johyun <porory415@gmail.com>
2021-08-05 17:07:20 +0900

    [21.08.05] continuous crawler coding

--- code/crawler_ignore_jh.py
+++ code/crawler_ignore_jh.py
@@ -1,30 +1,70 @@
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import pyautogui
 import time
 
-driver_path = '../resource/exe/chromedriver.exe'
+driver_path = '../../resource/exe/chromedriver.exe'
 url = 'https://www.google.co.kr/maps/@19.0753242,72.7389776,11z?hl=ko'
 
 search_name = "hospital in mumbai"
-browser = webdriver.Chrome(executable_path=driver_path) #Chrome driver
+browser = webdriver.Chrome(executable_path = driver_path) #Chrome driver
 browser.get(url)
 
 page = browser.page_source
 soup = BeautifulSoup(page, "html.parser")   #html파싱하겠다~
+data1 = []
+data2 = []
 
 def searchPlace():
     searchBox = browser.find_element_by_id("searchboxinput")
     searchBox.send_keys(search_name)
-    submitClick = browser.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button").click()
+    submit = browser.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button")
+    submit.click()
     time.sleep(2)
 
+    pyautogui.moveTo(178, 429)  #마우스 좌표 이동
+
+    # while True:
+    #     SCROLL_PAUSE_SEC = 1
+    #     # 끝까지 스크롤 다운
+    #     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #
+    #     # 1초 대기
+    #     time.sleep(SCROLL_PAUSE_SEC)
+    #
+    #     # 스크롤 다운 후 스크롤 높이 다시 가져옴
+    #     new_height = browser.execute_script("return document.body.scrollHeight")
+    #     last_height = new_height
+    #     if new_height == last_height:
+    #         break
+
+
 searchPlace()
+# 끝까지 스크롤 다운
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(2)
+browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-def scrapperPlace():
-    search_output = {'Title':[],'   Address':[  ]}
-    browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")  #스크롤 끝까지 다운
+for i in range(22):
+    try:
+        html = browser.page_source
+        soup = BeautifulSoup(html, features="lxml")
+        tmp = soup.select('.a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd')  # 한 페이지에 병원 정보 20개 있으니까 20개 찾아짐
+        data1.extend(tmp)  # data라는 리스트에 추가
+    except:
+        break
+# print(data1)
 
-    select_hospt = browser.find_elements_by_css_selector("a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd")
+dataList = []
+for tmp in data1:   # data1에서 하나씩 뽑아온 다음에
+    data_dict = {'name' : tmp['aria-label'], 'link': tmp['href']}  # 병원이름과 병원링크를 딕셔너리로 저장한 뒤
+    dataList.append(data_dict)   # 리스트에 추가.
+print(dataList)
+# data_df = pd.DataFrame(data1)
+# data_df.head(10)
+# dataList.to_csv('./hospital list.csv')
+
+##병원 하나하나 주소 가져오기
 
 
--- code/crawler_jh.py
+++ code/crawler_jh.py
@@ -1,56 +1,70 @@
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import pyautogui
 import time
 
-driver_path = '../resource/exe/chromedriver.exe'
+driver_path = '../../resource/exe/chromedriver.exe'
 url = 'https://www.google.co.kr/maps/@19.0753242,72.7389776,11z?hl=ko'
 
 search_name = "hospital in mumbai"
-browser = webdriver.Chrome(executable_path=driver_path) #Chrome driver
+browser = webdriver.Chrome(executable_path = driver_path) #Chrome driver
 browser.get(url)
 
-# get page html 전체 따옴
 page = browser.page_source
-# browser.quit() # 코드
 soup = BeautifulSoup(page, "html.parser")   #html파싱하겠다~
-# print(soup) #코드
-
+data1 = []
+data2 = []
 
 def searchPlace():
     searchBox = browser.find_element_by_id("searchboxinput")
     searchBox.send_keys(search_name)
-    submit = browser.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button").click()
+    submit = browser.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button")
+    submit.click()
     time.sleep(2)
 
+    pyautogui.moveTo(178, 429)  #마우스 좌표 이동
+
+    while True:
+        SCROLL_PAUSE_SEC = 1
+        # 끝까지 스크롤 다운
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # 1초 대기
+        time.sleep(SCROLL_PAUSE_SEC)
+
+        # 스크롤 다운 후 스크롤 높이 다시 가져옴
+        new_height = browser.execute_script("return document.body.scrollHeight")
+        last_height = new_height
+        if new_height == last_height:
+            break
+
+
 searchPlace()
+for i in range(22):
+    try:
+        html = browser.page_source
+        soup = BeautifulSoup(html, features="lxml")
+        tmp = soup.select('.a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd')  # 한 페이지에 병원 정보 20개 있으니까 20개 찾아짐
+        data1.extend(tmp)  # data라는 리스트에 추가
+    except:
+        break
+print(data1)
 
+i=0
+while i <= len(data1):
+    html = browser.page_source
+    soup = BeautifulSoup(html, features="lxml")
+    tmp2 = soup.select('aria-label')#
+    time.sleep(1)
+    data2.extend(tmp2)
+    i += 1
 
-def scrapperPlace():
-    search_output = {'Title':[],'   Address':[  ]}
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")  #스크롤 다운
-    select_hospt = browser.find_elements_by_css_selector("a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd")
+print(data2)
+data_df = pd.DataFrame(data2)
+data_df.head(10)
+# dataList.to_csv('./hospital list.csv')
 
-    for each_hosp in select_hospt:
-        each_hosp.click()
-        time.sleep(5)  #병원 리스트 중 한개 클릭
-
-        select_name = browser.find_element_by_css_selector("x3AX1-LfntMc-header-title-title gm2-headline-5")
-        search_output.Title.append(select_name)
-        select_name.click()
-        time.sleep(5)  #개별 병원 클릭 후 상호명 가져옴
-
-        select_adds = browser.find_element_by_css_selector("QSFF4-text gm2-body-2")
-        search_output.Address.append(select_name)
-        select_adds.click()
-        time.sleep(5)  # 개별 병원의 주소 가져옴
-
-    return search_output
-scrapperPlace()
-
-output = scrapperPlace()
-scrapper = pd.DataFrame(output)
-search_name = search_name.replace(" ","_")
-scrapper.to_csv(search_name+".csv", index=False)
+##병원 하나하나 주소 가져오기
 
 
