7a7d5c0c70955e3fceb9d094aae23d7bf654ed99
Johyun <porory415@gmail.com>
2021-08-11 16:49:13 +0900

    [21.08.11]

--- code/Cleansing/cleansed_google_data.py
+++ code/Cleansing/cleansed_google_data.py
@@ -1,7 +1,8 @@
 import os
+import os.path
 import pandas as pd
 
-path = '../../resource/CrawlingData/'
+path = r'../../CrawlingData/'
 details = ['hospital/', 'clinic/', 'fitness/']
 
 # 1. 각 csv 파일 내 중복 제거
@@ -10,23 +11,29 @@ for detail in details:
     csv_list = os.listdir(full_path)
     for data in csv_list:
         raw_data = pd.read_csv(data)
-        print(data, 'raw데이터 수:', len(raw_data))
-        new_data = raw_data.drop_duplicates(['Url'])
-        print('\t수정 데이터 수:', len(new_data))
+        print('raw데이터 수:', len(raw_data))
+        new_data = raw_data.drop_duplicates(['Url'])   #url이 중복 발견의 기준
+        print('수정 데이터 수:', len(new_data))
 
 
-# 2. 안맞는 카테고리 제거 + 더미데이터(주소/카테고리:null) 제거
+# 2. 안맞는 카테고리 제거 + 더미데이터(url&카테고리:null) 제거;
 # hospital(animal, eye)
 hospital_list = os.listdir(path+details[0])
-for data in hospital_list:
-    hospital = pd.read_csv(data)
+
+
 
 # clinic()
 clinic_list = os.listdir(path+details[1])
-for data in clinic_list:
-    clinic = pd.read_csv(data)
+for csv_data in clinic_list:
+    clinic = pd.read_csv(csv_data)
+    for idx in clinic['Category']:
+        idx = clinic[clinic['Category'] == 'Hair' or 'Eye' or 'Dental','Beauty' or 'Counselor' or 'Animal' or
+        'pet' or 'Dentist' or 'mall' or 'Hotel'].index
+        clinic.drop(idx, inplace=True)
+        idx = clinic[clinic['Category'] == 'Family' or 'Dermatolo' or 'pediatrician' or 'Hair' or ''
+        rowth2 = rowth2.extend(idx)
 
 # fitness(equipment? store? )
 fitness_list = os.listdir(path+details[2])
-for data in fitness_list:
-    fitness = pd.read_csv(data)
+for csv_data in fitness_list:
+    fitness = pd.read_csv(csv_data)
