c7a93255b93bdad8ad028fd8e18b0a74c2c36e06
MinYeong-37 <min__37@naver.com>
2021-08-11 19:49:11 +0900

    [21.08.10] geocoding_modified_MY

--- code/Crawler/geocoding.py
+++ code/Crawler/geocoding.py
@@ -1,26 +1,30 @@
+import os
 import pandas as pd
 
-keyw = 'clinic'
-state = 'Assam'
-keyw = pd.read_csv(r'C:\Users\Lenovo\PycharmProjects\new_cleansing\resource\CrawlingData\hospital in Uttarakhand, India.csv')
-
-print(keyw.shape)
-keyw.head()
-
-
 def find_latitude_longtitude(url):
-    latitude_start_index = url.find('!3d')
-    latitude_end_index = url.find('!4d')
-    longtitude_start_index = url.find('!4d')
-    longtitude_end_index = url.find('?')
+    latitude_start_index = str(url).find('!3d')
+    latitude_end_index = str(url).find('!4d')
+    longtitude_start_index = str(url).find('!4d')
+    longtitude_end_index = str(url).find('?')
 
-    latitude = url[latitude_start_index + 3: latitude_end_index]
-    longtitude = url[longtitude_start_index + 3: longtitude_end_index]
+    latitude = str(url)[latitude_start_index + 3: latitude_end_index]
+    longtitude = str(url)[longtitude_start_index + 3: longtitude_end_index]
 
-    return float(latitude), float(longtitude)
+    return latitude, longtitude
+    # return float(latitude), float(longtitude)
 
-keyw['latitude'], keyw['longtitude'] = zip(*keyw['Url'].apply(find_latitude_longtitude))
-keyw.head()
+# Main
+origin_dir = '../../resource/CleansedData/GoogleMap/'
+for detail in os.listdir(origin_dir):
+    # detail = ['Clinic','Fitness','Hospital']
+    for csv_data in os.listdir(origin_dir + detail):
+        file_name = os.path.basename(csv_data) + '_final.csv'
+        data = pd.read_csv(origin_dir + detail + '/' + csv_data)
+        # print(data.shape)
+        # data.head()
 
-save_dir = r'C:/Users/Lenovo/PycharmProjects/new_cleansing/resource/GeocodingData/hospital in Uttarakhand'+'.csv'
-keyw.to_csv(save_dir, index=False)
+        data['latitude'], data['longtitude'] = zip(*data['Url'].apply(find_latitude_longtitude))
+        # data.head()
+
+        save_dir = '../../resource/GeocodingData/' + detail + '/' + file_name
+        data.to_csv(save_dir, index=False)
