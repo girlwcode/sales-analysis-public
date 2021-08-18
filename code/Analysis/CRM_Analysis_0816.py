1cf6b843100a759eecf3bb1bcc5d910886df7d84
yelyn Ahn <yelynahn@gmail.com>
2021-08-18 17:15:43 +0900

    [21.08.18 ]CRM data Analysis and Plots_YL

--- code/Analysis/CRM_Analysis_0816.py
+++ code/Analysis/CRM_Analysis_0816.py
@@ -11,7 +11,13 @@ sales_21 = pd.read_csv('../../resource/SalesData/Installation_2021.csv')
 leads = pd.read_csv('../../resource/CleansedData/Zoho CRM/Leads_001_IndustryCleansed.csv')
 deals = pd.read_csv('../../resource/CleansedData/Zoho CRM/Potentials_001_fillTerritories_final.csv')
 
-print(sales_17.columns)
+#Index(['S. No.', 'Month', 'Client', 'City', 'State', 'Region', 'Category',
+      #  'Sales Official', 'Installation Date', 'Order/ Model Details',
+      #  'Serial No.', 'Invoice Details', 'Actual Value', 'Tax ', 'Total',
+      #  'Payment Status (Rs) ', 'Payment Status', 'Installation Status',
+      #  'Warraty', 'installation', '6month visit'],
+      # dtype='object')
+# print(sales_17.columns)
 
 # 순 이익 : 17,18 - Actual Value, 19,20,21 - Net
 revenue_17 = sales_17.loc[:,['Month','Region','Category','Actual Value']]
@@ -19,6 +25,7 @@ revenue_18 = sales_18.loc[:,['Month','Region','Category','Actual Value']]
 revenue_19 = sales_19.loc[:,['Month','Region','Category','Net']]
 revenue_20 = sales_20.loc[:,['Month','Region','Category','Net']]
 revenue_21 = sales_21.loc[:,['Month','Region','Category','Net']]
+
 
 # Lead, Deal 월별로 구분
 lead = leads.loc[:,['Lead Source','Lead Status','Created Time','Industry Fin']]
@@ -35,8 +42,8 @@ lead['month'] = pd.DatetimeIndex(lead['Created Time']).month
 deal['year'] = pd.DatetimeIndex(deal['Created Time']).year
 deal['month'] = pd.DatetimeIndex(deal['Created Time']).month
 
-print(lead)
-print(deal)
+# print(lead)
+# print(deal)
 
 # 컬럼명 변경
 revenue_17.rename(columns = {'Actual Value' : 'Net'}, inplace = True)
@@ -50,12 +57,67 @@ revenue_19['Net'] = revenue_19['Net'].fillna(0)
 revenue_20['Net'] = revenue_20['Net'].fillna(0)
 revenue_21['Net'] = revenue_21['Net'].fillna(0)
 
+# col에 년도 추가
+revenue_17['Year'] = 2017
+revenue_18['Year'] = 2018
+revenue_19['Year'] = 2019
+revenue_20['Year'] = 2020
+revenue_21['Year'] = 2021
+
+# col 순서 변경
+revenue_17= revenue_17[['Year','Month','Region','Category','Net']]
+revenue_18= revenue_18[['Year','Month','Region','Category','Net']]
+revenue_19= revenue_19[['Year','Month','Region','Category','Net']]
+revenue_20= revenue_20[['Year','Month','Region','Category','Net']]
+revenue_21= revenue_21[['Year','Month','Region','Category','Net']]
+
 # print(revenue_17['Region'].unique())
 
 years = [revenue_17, revenue_18, revenue_19, revenue_20, revenue_21]
 months = ['January','February','March','April','May','June','July','August','September','October','November','December']
 regions = ['West 1','South','North','East','West 2']
 rev_year = {}
+
+# revenue_yearly
+plt.figure(figsize=(10, 7))
+plt.title('Whole Revenue of Inbody,India', fontsize=18)
+x = ['2017','2018','2019','2020','2021']
+y = [round(sum(revenue_17['Net']),2),round(sum(revenue_18['Net']),2),round(sum(revenue_19['Net']),2),round(sum(revenue_20['Net']),2),round(sum(revenue_21['Net']),2)]
+plt.plot(x,y)
+for i, x_ in enumerate(x):
+    plt.text(x_, y[i], y[i],
+             fontsize=10,
+             color='black',
+             horizontalalignment='center',
+             verticalalignment='top')
+
+plt.savefig('../../resource/Plot/Whole Revenue of Inbody,India (Yearly).png')
+plt.show()
+
+# revenue_monthly,
+year = 2017
+cnt = 1
+for y in years :
+    plt.cla()
+    # print(year)
+    revenue = {}
+    mt = 1
+    if (cnt == 5):
+        months =  ['January','February','March','April','May','June','July']
+    for m in months :
+        r = sum(y[y['Month'] == m]['Net'])
+        revenue[m] = r
+        key = str(year) + '.' + str(mt)
+        rev_year[key] = r
+        mt += 1
+    # print(revenue)
+    # title = 'Revenue of '+str(year)
+    # plt.figure(figsize=(12, 7))
+    # plt.title(title, fontsize=15)
+    # plt.plot(list(revenue.keys()), list(revenue.values()))
+    # plt.savefig('../../resource/Plot/'+title+'(monthly).png')
+    year += 1
+    cnt += 1
 
 # revenue_monthly
 year = 2017
@@ -102,74 +164,77 @@ for y in years :
     # plt.savefig('../../resource/Plot/'+title+'(territory).png')
 
 
-# lead 갯수
-l = {}
-year = [2017,2018,2019,2020,2021]
-month = [1,2,3,4,5,6,7,8,9,10,11,12]
-for y in year:
-    if (y == 2021):
-        month = [1, 2, 3, 4, 5, 6, 7]
-    for m in month:
-        cnt = 0
-        for row in lead.index :
-            if (lead['year'][row] == y and lead['month'][row] == m) :
-                cnt +=1
-            key = str(y) +'.'+str(m)
-            l[key] = cnt
+# # lead 갯수
+# l = {}
+# year = [2017,2018,2019,2020,2021]
+# month = [1,2,3,4,5,6,7,8,9,10,11,12]
+# for y in year:
+#     if (y == 2021):
+#         month = [1, 2, 3, 4, 5, 6, 7]
+#     for m in month:
+#         cnt = 0
+#         for row in lead.index :
+#             if (lead['year'][row] == y and lead['month'][row] == m) :
+#                 cnt +=1
+#             key = str(y) +'.'+str(m)
+#             l[key] = cnt
+#
+# # deal 갯수
+# d = {}
+# year = [2017,2018,2019,2020,2021]
+# month = [1,2,3,4,5,6,7,8,9,10,11,12]
+# for y in year:
+#     if (y == 2021):
+#         month = [1, 2, 3, 4, 5, 6, 7]
+#     for m in month:
+#         cnt = 0
+#         for row in deal.index :
+#             if (deal['year'][row] == y and deal['month'][row] == m) :
+#                 cnt +=1
+#             key = str(y) +'.'+str(m)
+#             d[key] = cnt
+#
+# plt.plot(list(d.keys()), list(d.values()))
+# plt.title('Whole Deals per months of Inbody, India', fontsize=18)
+# plt.xticks(rotation=90)
+# plt.show()
+#
+# # Whole revenue
+# x = list(rev_year.keys())
+# y1 = list(rev_year.values())
+# y2 = list(l.values())
+#
+# fig, ax1 = plt.subplots()
+# ax1.plot(x, y1, color='deeppink', label='Revenue')
+# fig.autofmt_xdate(rotation=90)
+# ax1.legend(loc='upper right')
+#
+# ax2 = ax1.twinx()
+# ax2.plot(x, y2, color='green', label='Leads')
+# ax1.legend(loc='lower right')
+#
+# plt.title('Whole Revenue and leads of Inbody, India', fontsize=18)
+# plt.legend()
+# plt.show()
+#
+# # Leads and Deals
+# x = list(d.keys())
+# y1 = list(d.values())
+# y2 = list(l.values())
+#
+# fig, ax1 = plt.subplots()
+# ax1.plot(x, y1, color='deeppink', label='Deals')
+# fig.autofmt_xdate(rotation=90)
+# ax1.legend(loc='upper right')
+#
+# ax2 = ax1.twinx()
+# ax2.plot(x, y2, color='green', label='Leads')
+# ax1.legend(loc='lower right')
+#
+# plt.title('Whole Deals and leads of Inbody, India', fontsize=18)
+# plt.legend()
+# plt.show()
 
-# deal 갯수
-d = {}
-year = [2017,2018,2019,2020,2021]
-month = [1,2,3,4,5,6,7,8,9,10,11,12]
-for y in year:
-    if (y == 2021):
-        month = [1, 2, 3, 4, 5, 6, 7]
-    for m in month:
-        cnt = 0
-        for row in deal.index :
-            if (deal['year'][row] == y and deal['month'][row] == m) :
-                cnt +=1
-            key = str(y) +'.'+str(m)
-            d[key] = cnt
-
-plt.plot(list(d.keys()), list(d.values()))
-plt.title('Whole Deals per months of Inbody, India', fontsize=18)
-plt.xticks(rotation=90)
-plt.show()
-
-# Whole revenue
-x = list(rev_year.keys())
-y1 = list(rev_year.values())
-y2 = list(l.values())
-
-fig, ax1 = plt.subplots()
-ax1.plot(x, y1, color='deeppink', label='Revenue')
-fig.autofmt_xdate(rotation=90)
-ax1.legend(loc='upper right')
-
-ax2 = ax1.twinx()
-ax2.plot(x, y2, color='green', label='Leads')
-ax1.legend(loc='lower right')
-
-plt.title('Whole Revenue and leads of Inbody, India', fontsize=18)
-plt.legend()
-plt.show()
-
-# Leads and Deals
-x = list(d.keys())
-y1 = list(d.values())
-y2 = list(l.values())
-
-fig, ax1 = plt.subplots()
-ax1.plot(x, y1, color='deeppink', label='Deals')
-fig.autofmt_xdate(rotation=90)
-ax1.legend(loc='upper right')
-
-ax2 = ax1.twinx()
-ax2.plot(x, y2, color='green', label='Leads')
-ax1.legend(loc='lower right')
-
-plt.title('Whole Deals and leads of Inbody, India', fontsize=18)
-plt.legend()
-plt.show()
-
+# concat the revenues
+df = pd.concat([revenue_17,revenue_18,revenue_19,revenue_20,revenue_21],axis=0,  ignore_index=True)
+df.to_csv('../../resource/SalesData/Whole Revenue.csv', index=False)
--- code/Analysis/CRM_leads_deals_Analysis_0817.py
+++ code/Analysis/CRM_leads_deals_Analysis_0817.py
@@ -9,10 +9,21 @@ sales_dir = '../../resource/SalesData/'
 leads = pd.read_csv('../../resource/CleansedData/Zoho CRM/Leads_001_IndustryCleansed.csv')
 deals = pd.read_csv('../../resource/CleansedData/Zoho CRM/Potentials_001_fillTerritories_final.csv')
 installed = pd.read_csv('../../resource/SalesData/Install_full.csv')
+revenue = pd.read_csv('../../resource/SalesData/Whole Revenue.csv')
 
 # Lead, Deal 월별로 구분
 lead = leads.loc[:, ['Lead Source', 'Lead Status', 'Created Time', 'Industry Fin']]
 deal = deals.loc[:, ['Amount', 'Stage', 'Lead Source', 'Created Time', 'Territory_fin']]
+
+# Unique key
+lead_source = pd.DataFrame(lead['Lead Source'].unique(), columns=['Lead Source'])
+lead_Status = pd.DataFrame(lead['Lead Status'].unique(), columns=['Lead Status'])
+stage = pd.DataFrame(deal['Stage'].unique(), columns=['Deal Stage'])
+deal_source = pd.DataFrame(deal['Lead Source'].unique(), columns=['Deal Lead Source'])
+territory = pd.DataFrame(deal['Territory_fin'].unique(), columns=['Territory'])
+industry = pd.DataFrame(lead['Industry Fin'].unique(), columns=['Industry'])
+df2 = pd.concat([lead_source,lead_Status,stage,deal_source,territory,industry], axis=0, ignore_index=True)
+# df2.to_csv('점수.csv',index=False)
 
 # 날짜 DateTime으로 변경
 lead['Created Time'] = pd.to_datetime(lead['Created Time'])
@@ -25,8 +36,83 @@ lead['month'] = pd.DatetimeIndex(lead['Created Time']).month
 deal['year'] = pd.DatetimeIndex(deal['Created Time']).year
 deal['month'] = pd.DatetimeIndex(deal['Created Time']).month
 
-deal_closed = collections.Counter(installed['Year'])
-print(deal_closed)
+# 한 해 Customer
+installed_company = collections.Counter(installed['Year'])
+# print(installed_company)
+
+# Rev 월 변경
+months = {'January':1,'February':2,'March':3,'April':4,'May':5,'June':6,'July':7,'August':8,'September':9,
+          'October':10,'November':11,'December':12}
+for row in revenue.index :
+    for month in months.keys():
+        if (revenue['Month'][row] == month):
+            revenue['Month'][row] = months[month]
+
+# 년도/월별 매출
+rev_year = {2017:[],2018:[],2019:[],2020:[],2021:[]}
+rev_month ={2017:[],2018:[],2019:[],2020:[],2021:[]}
+
+x = []
+for y in rev_year.keys() :
+    ry = sum(revenue[revenue['Year'] == y]['Net'])
+    rev_year[y] = ry
+    for m in range (1,13) :
+        condition1 = revenue['Year'] == y
+        condition2 = revenue['Month'] == m
+        x.append(str(y)+"."+str(m))
+        rm = round(sum(revenue[condition1 & condition2]['Net']),2)
+        rev_month[y].append(rm)
+
+print(rev_month.values())
+
+# Converted Rate - Monthly
+number_lead = lead.groupby(['year', 'month']).size().reset_index()
+number_deal = deal.groupby(['year', 'month']).size().reset_index()
+
+number_lead.rename(columns = {0 : 'lead'}, inplace = True)
+number_deal.rename(columns = {0 : 'deal'}, inplace = True)
+
+monthly_num = pd.concat([number_lead,number_deal['deal']],axis=1, ignore_index=True)
+monthly_num.rename(columns = {0 : 'year',1 : 'month',2 : 'lead',3 : 'deal'}, inplace = True)
+monthly_num['converted Rate'] = monthly_num['deal'] / (monthly_num['lead'] + monthly_num['deal']) * 100
+
+# revenue per month and converted rate
+# y1 = list(rev_year.values())
+# y2 = list(installed_company.values())
+#
+# fig, ax1 = plt.subplots()
+# ax1.plot(x, y1, color='deeppink', label='Revenue')
+# fig.autofmt_xdate(rotation=90)
+# ax1.legend(loc='upper right')
+#
+# ax2 = ax1.twinx()
+# ax2.plot(x, y2, color='green', label='Customer')
+# ax1.legend(loc='lower right')
+#
+# plt.title('Whole Revenue and Customer of Inbody, India', fontsize=18)
+# plt.legend()
+# plt.show()
+
+
+
+# revenue and Installed Company
+# x = list(rev_year.keys())
+# y1 = list(rev_year.values())
+# y2 = list(installed_company.values())
+#
+# fig, ax1 = plt.subplots()
+# ax1.plot(x, y1, color='deeppink', label='Revenue')
+# fig.autofmt_xdate(rotation=90)
+# ax1.legend(loc='upper right')
+#
+# ax2 = ax1.twinx()
+# ax2.plot(x, y2, color='green', label='Customer')
+# ax1.legend(loc='lower right')
+#
+# plt.title('Whole Revenue and Customer of Inbody, India', fontsize=18)
+# plt.legend()
+# plt.show()
+
 
 # Deal stage unique
 # '10.Archive': 951, '7. Deal Closed (Payment done)': 532, '6. Installation': 431, '9. Rejected': 141, '3. B- Negotiation': 116,
