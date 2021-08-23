fae46304bbce928c23466770028f02edd7abd7cf
yelyn Ahn <yelynahn@gmail.com>
2021-08-23 17:05:10 +0900

    [21.08.23] plot the hdi_YL

--- code/Data Preprocessing/Exponential_Smoothing_HDI,GDP_0823.py
+++ code/Data Preprocessing/Exponential_Smoothing_HDI,GDP_0823.py
@@ -8,4 +8,20 @@ hdi = pd.read_csv('../../resource/External_Feature/HDI_India.csv')
 #  'Jammu and Kashmir', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland',
 #  'New Delhi', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana',
 #  'Tripura', 'Uttar Pradesh', 'Uttaranchal', 'West Bengal']
+
 print(hdi['State'].tolist())
+
+x = range(1990, 2020)
+y = []
+count = 0
+for i in hdi.index:
+    if count == 4:
+        break
+    y = hdi.iloc[i,:].tolist()
+    state = y.pop(0)
+    plt.plot(x, y)
+    plt.title(state+'_HDI')
+    # plt.show()
+    plt.savefig('../../resource/Plot_External/'+state+'_hdi.png')
+    plt.cla()
+    count+=1
